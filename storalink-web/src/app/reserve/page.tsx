283c199fea784070c2491cb9a92aabf1d7842481
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-12-07 15:47:43 -0500

    small update

--- storalink-web/src/app/reserve/page.tsx
+++ storalink-web/src/app/reserve/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import theme from "../theme";
 import { styled } from "styled-components";
 import Image from "next/image";
 import reserve_1 from "../../../public/reserve_1.png";
 import reserve_2 from "../../../public/reserve_2.png";
+import mailBox from "../../../public/mailbox.svg";
 const UserInputBox = styled.input`
   border: 2px solid ${theme.themeDarkGrey};
   padding-left: 0.6rem;
@@ -24,7 +25,29 @@ const UserInputArea = styled.textarea`
   border-radius: ${theme.mediumRadius};
   width: 100%;
 `;
+
+const SuccessDiv = styled.div`
+  border: 2px solid #03ac00;
+  padding-left: 0.6rem;
+  padding-right: 0.6rem;
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
+  border-radius: ${theme.mediumRadius};
+  width: 100%;
+  background-color: #f7fff6;
+
+  p {
+    color: #03ac00;
+    padding-left: 0.6rem;
+    padding-right: 0.6rem;
+    padding-top: 0.3rem;
+    padding-bottom: 0.3rem;
+    text-align: center;
+  }
+`;
+
 function About() {
+  const [submit, setSubmit] = useState(false);
   return (
     <div className="flex justify-between">
       <div className="md:w-2/5 min-h-screen">
@@ -44,32 +67,59 @@ function About() {
           beta version of the mobile app is ready, you will receive an email
           with more information!
         </div>
-        <div>
-          <div className="text-sm">Name*</div>
-          <UserInputBox type="text" className="mb-4" />
-          <div className="text-sm">Email*</div>
-          <UserInputBox type="text" className="mb-4" />
-
-          <div className="text-sm">How did you hear about us</div>
-          <UserInputArea />
-
-          <div className="flex items-center">
-            <input type="checkbox" style={{border: `2px solid ${theme.themeYellow}; margin-end: 5px;`}}/>
-            <p className="ms-2">I have access to Apple store </p>
-          </div>
-
+        {submit ? (
+          <>
+            <SuccessDiv>
+              <p>Congrats! You have been added to the list!</p>
+            </SuccessDiv>
+            <Image
+              src={mailBox}
+              alt="reserve image two"
+              fill={false}
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </>
+        ) : (
           <div>
-            <button
-              className="px-5 py-2 mt-5 rounded-md w-80"
-              style={{ color: theme.themeWhite, backgroundColor: theme.themeYellow }}
-            >
-              Get on the list 
-            </button>
+            <div className="text-sm">Name*</div>
+            <UserInputBox type="text" className="mb-4" />
+            <div className="text-sm">Email*</div>
+            <UserInputBox type="text" className="mb-4" />
+
+            <div className="text-sm">How did you hear about us</div>
+            <UserInputArea />
+
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                style={{
+                  border: `2px solid ${theme.themeYellow}; margin-end: 5px;`,
+                }}
+              />
+              <p className="ms-2">I have access to Apple store </p>
+            </div>
+
+            <div>
+              <button
+                className="px-5 py-2 mt-5 rounded-md w-80"
+                style={{
+                  color: theme.themeWhite,
+                  backgroundColor: theme.themeYellow,
+                }}
+                onClick={() => {
+                  setSubmit(true);
+                }}
+              >
+                Get on the list
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
-      <div className="md:w-3/5  md:absolute right-0 top-0">
+      <div className="w-full md:w-3/5 lg:absolute right-0 top-0 hidden sm:block">
         <Image
           src={reserve_1}
           width={700}
